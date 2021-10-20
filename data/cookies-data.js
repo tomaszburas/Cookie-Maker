0b9c23e4ace485b4af0baa2e66c81b02c48c7a09
Tomasz Buras <tomaszburas.dev@gmail.com>
2021-10-20 22:15:20 +0200

    edit front

---
+++ data/cookies-data.js
@@ -0,0 +1,25 @@
+const COOKIE_BASES = {
+    light: 3,
+    dark: 3,
+};
+
+const COOKIE_GLAZE = {
+    honey: 2.5,
+    chocolate: 2,
+}
+
+const COOKIE_ADDONS = {
+    sprinkles: 1,
+    nuts: 1.5,
+    cherry: 2,
+    kiwi: 2,
+    orange: 2,
+    strawberries: 2,
+    watermelon: 2,
+}
+
+module.exports = {
+    COOKIE_BASES,
+    COOKIE_GLAZE,
+    COOKIE_ADDONS
+}
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const {COOKIE_BASES, COOKIE_GLAZE, COOKIE_ADDONS} = require("../data/cookies-data");
 
 const homeRouter = express.Router();
 
 homeRouter
-    .get('/', (req, res) => res.render('home/index'))
+    .get('/', (req, res) => {
+        res.render('home/index', {
+            cookie: {
+                base: 'light',
+                glaze: 'chocolate',
+                addons: ['kiwi'],
+            },
+            bases: Object.entries(COOKIE_BASES),
+            glazes: Object.entries(COOKIE_GLAZE),
+            addons: Object.entries(COOKIE_ADDONS)
+        });
+    });
 
 module.exports = {
     homeRouter
